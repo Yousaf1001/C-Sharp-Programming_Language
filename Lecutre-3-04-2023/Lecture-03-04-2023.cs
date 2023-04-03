0fefa9453de1e8c1056c1ef025c4e859bd2ae676
Yousaf1001 <yousimalik1012@gmail.com>
2023-04-03 05:14:59 -0700

    Read from one file and Write another file (Duplication

--- Lecutre-3-04-2023/Lecture-03-04-2023.cs
+++ Lecutre-3-04-2023/Lecture-03-04-2023.cs
@@ -5,14 +5,35 @@ namespace Lecture_03_04_2023
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            FileStream fin = new FileStream("path.txt", FileMode.Create);
-            for(char c='A'; c<'Z'; c++)
-            {
-                fin.WriteByte((byte)c);
-            }
+        //Write Bytes in  file
+        // static void Main(string[] args)
+        // {
+        //     FileStream fin = new FileStream("path.txt", FileMode.Create);
+        //     for(char c='A'; c<'Z'; c++)
+        //     {
+        //         fin.WriteByte((byte)c);
+        //     }
            
+        //     fin.Close();
+        // }
+
+        //Read  Data from one file wrote another file(Duplication) 
+         static void Main(string[] args)
+        {
+            FileStream fin = new FileStream("path.txt", FileMode.Open);
+            FileStream fin1 = new FileStream("path1.txt", FileMode.Create);
+            /* for(char c='A';c<='Z'; c++)
+             {
+                 fin.WriteByte((byte)c);
+             }*/
+            int i  = fin.ReadByte(); 
+            while (i != -1)
+            {
+                char c = (Char)i;
+                fin1.WriteByte((byte)c);
+                i = fin.ReadByte();
+            }
+
             fin.Close();
         }
     }
